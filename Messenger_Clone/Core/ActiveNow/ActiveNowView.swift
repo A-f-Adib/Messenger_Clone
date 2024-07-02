5b264b23f8ed2b97b55d904672092b2f66e11b5d
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-07-03 02:38:39 +0600

    updated

--- Messenger_Clone/Core/ActiveNow/ActiveNowView.swift
+++ Messenger_Clone/Core/ActiveNow/ActiveNowView.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct ActiveNowView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack (spacing: 32) {
+                ForEach(0...10, id: \.self) { user in
+                    VStack {
+                        ZStack(alignment: .bottomTrailing)  {
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                                .frame(width: 64, height: 64)
+                                .foregroundColor(.gray)
+                            
+                            ZStack{
+                                Circle()
+                                    .fill(.white)
+                                    .frame(width: 18, height: 18)
+                                Circle()
+                                    .fill(.green)
+                                    .frame(width: 12, height: 12)
+                            }
+                        }
+                        Text("Bruce")
+                            .foregroundColor(.gray)
+                            .font(.subheadline)
+                    }
+                }
+            }
+            .padding()
+        }
+        .frame(height: 106)
     }
 }
 
