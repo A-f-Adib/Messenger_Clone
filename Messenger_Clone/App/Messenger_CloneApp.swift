5a77ab370c6d7c737d655cb82b7eb1050160bef6
A.F. Adib <a.f.adib36@gmail.com>
2024-08-21 22:42:16 -0700

    firebase added

--- Messenger_Clone/App/Messenger_CloneApp.swift
+++ Messenger_Clone/App/Messenger_CloneApp.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 import FirebaseCore
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
 @main
 struct Messenger_CloneApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
